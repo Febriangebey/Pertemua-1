cecac6cbfdec5a2b47e64940e67cd1c0bbbe1944
Febriangebey <febriangebey@gmail.com>
2022-03-10 15:31:52 +0700

    Code tentang nilai Var pada kotlin

---
+++ src/main/kotlin/Var.kt
@@ -0,0 +1,7 @@
+fun main(){
+    var varVariabel: Int
+    varVariabel = 56 //assinging first value to varVariabel
+    println("Pembrian nilai pertama pada varVariabel = " + varVariabel)
+    varVariabel = 78 //assigning second value to varVariabel
+    println("Pemberian nilai kedua pada varVariabel = "+varVariabel)
+}
