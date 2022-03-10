d2689715bbf4b0b9dfa9df3f3493aa5e49b82f70
Febriangebey <febriangebey@gmail.com>
2022-03-10 15:22:29 +0700

    Code tentang Variabel di kotlin

---
+++ src/main/kotlin/Varaibel.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = " + dynamicVar)
+
+    var staticVar: Char = 'A'
+    println("Contoh static variabel = " + staticVar )
+
+}
