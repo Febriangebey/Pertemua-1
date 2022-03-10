935e37fe505a4d6fcef92275ab985ab7e7faeddf
Febriangebey <febriangebey@gmail.com>
2022-03-10 15:07:31 +0700

    Code tentang whitespace di kotlin

---
+++ src/main/kotlin/Blok dan Komentar.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Hello ini program Saya di Kotlin")
+
+    //contoh komentar satu baris
+    /*
+    contoh komentar multi baris
+     */
+    /**
+     * kotlin Doc -->baris ini mendukumentasikan program
+     */
+}
